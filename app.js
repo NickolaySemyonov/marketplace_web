e0b79b1286b550ec76a9cb56d57fee142582e734
nikol <nikolaysem2004@gmail.com>
2024-10-12 21:55:19 +0300

    basic server code

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = 3000;
+
+__dirname = path.join(__dirname, 'public');
+// Serve static files from the "public" directory
+app.use(express.static(__dirname));
+
+// Route to serve your HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
